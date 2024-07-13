c16b3242627a00b17cc1eb745a1a8ec0132c6d10
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-13 19:40:23 +0300

    Added arguments in the command line

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Hello World!')
+function main() {
+    const args = process.argv.slice(2);
+
+    if (args.length < 1) {
+      console.error('Error: Not enough arguments. Please provide a base URL.');
+      process.exit(1);
+    }
+  
+    if (args.length > 1) {
+      console.error('Error: Too many arguments. Please provide only one base URL.');
+      process.exit(1);
+    }
+
+    const baseURL = args[0];
+    console.log(`Starting the crawler at: ${baseURL}`);
+  }
+
+main()
