fda7c0adc3e9374611b03de992e6f584f7f42b64
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-07-13 18:18:34 +0300

    Function for extracting URLs, refined unit test for it

--- crawl.js
+++ crawl.js
@@ -11,4 +11,20 @@ function normalizeURL(url) {
     }
 }
 
-function getURLSFromHTML() {}
+function getURLSFromHTML(htmlBody, baseURL) {
+    const dom = new JSDOM(htmlBody);
+    const links = dom.window.document.querySelectorAll('a');
+    const urls = Array.from(links).map(link => link.href);
+    for (let index in urls) {
+        if (!urls[index].includes(baseURL)) {
+            urls[index] = baseURL + urls[index]
+        }
+
+        if (urls[index].endsWith('/')) {
+            urls[index] = urls[index].slice(0, -1)
+        }
+    }
+    return urls;
+}
+
+console.log(getURLSFromHTML(`<html><body><a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a></body></html>`, "https://blog.boot.dev"));
